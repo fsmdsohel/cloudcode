ef9fc61cc32c5f5387107b1f7aa02d442b40a751
fsmdsohel <sohel.rana.coder@gmail.com>
2025-12-30 21:17:11 +0600

    refactor: Migrate to pnpm, remove the test-prisma project, and update Prisma configuration.

--- apps/backend/src/controllers/prismaClient.ts
+++ apps/backend/src/controllers/prismaClient.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 // Create a singleton instance of PrismaClient
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
